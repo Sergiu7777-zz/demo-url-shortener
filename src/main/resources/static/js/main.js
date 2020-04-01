0a5d92381ae2a93dab0dce2113d9c5f3a2a56752
Sergiu7777 <serghei7777@gmail.com>
2020-04-01 16:41:07 +0300

    --added clipboard copy function

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,28 @@
+function copyLink() {
+    var copyText = document.getElementById("js-url-input");
+    var range = document.createRange();
+    range.selectNode(copyText);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied!";
+}
+
+function outFunction() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
+
+function resetAllFields() {
+    document.getElementById("js-url-input").innerHTML = "";
+}
+
+function toggleButton(ref,bttnID){
+    document.getElementById(bttnID).disabled= ((ref.value === ref.defaultValue));
+    // document.getElementById("js-copy-btn").disabled= ((ref.value === ref.defaultValue));
+}
+
+document.getElementById("js-url-input").addEventListener("click", displayDate);
